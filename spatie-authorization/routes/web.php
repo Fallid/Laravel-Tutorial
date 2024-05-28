163ee927e3c5aec011edb2da49beca449232a2a0
Ahmad Naufal <naufalopal002@gmail.com>
2024-05-28 23:32:45 +0700

    fix: Fix guest route

--- spatie-authorization/routes/web.php
+++ spatie-authorization/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'layouts.guest')->name('welcome');
+Route::view('/', 'welcome')->name('welcome');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified', 'role:user'])
